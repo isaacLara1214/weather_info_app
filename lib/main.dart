1bf1610b63e4122e1da994650c299e17dcec49e2
fheise1 <flynnheise@gmail.com>
2025-02-05 13:51:49 -0500

    used the flutter's website deafault textfield

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const TextField(
             ),
             Text(
               '$_counter',
